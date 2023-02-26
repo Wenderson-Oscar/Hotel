eba3bc6f514ef3a9e6955f919ed56333228d61a5
Wenderson-Oscar <wendersonoscardev@gmail.com>
2023-02-26 16:24:36 -0300

    mudando chave_pk

--- back-end/checkout.py
+++ back-end/checkout.py
@@ -22,13 +22,13 @@ class Checkout:
         cursor.execute("""INSERT INTO checkout (valor_consumo, valor_pago, data_criacao, pk_funcionario,
         pk_reserva, pk_reserva_hospede, pk_reserva_quarto, pk_reserva_categoria) VALUES (?,?,?,?,?,?,?,?)""",
         (self.valor_consumo, self.valor_pago, datetime.today(), count_employee_pk, count_reserve_pk, 
-        count_client_pk, count_room_pk, count_room_pk))
+        count_client_pk, count_room_pk, count_category_pk))
         conn.commit()
         conn.close()
         return 'Dados Inseridos'
     
     def calculate_value_total(self, automate_pk: AutoIncrementPk):
-        """faz o calculo de pagamento total considenrado diaria, multa, serviço"""
+        """faz o calculo de pagamento total considerando diaria, multa, serviço"""
         count_reserva_pk = automate_pk.count_reserve()
         conn = self.model.database.connect()
         cursor = conn.cursor()
@@ -48,9 +48,9 @@ class Checkout:
         LEFT JOIN reservar_servico rs ON rs.pk_categoria = c.id_categoria
         LEFT JOIN servico serv ON serv.id_servico = rs.pk_servico
         WHERE r.pk_hospede = ? """, (str(count_reserva_pk)))
-        result = cursor.fetchone()[0]
+        result = cursor.fetchone()
         conn.close()
-        return result if result else None
+        return result if result else 'Não a dados para calcular!'
 
 
 if __name__ == "__main__":
